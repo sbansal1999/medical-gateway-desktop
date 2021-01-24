b1bb6ca9298e035c76225ef3a14401ddc32a4862
Satyam Bansal <sbansal1999@gmail.com>
2021-01-24 15:36:09 +0530

    Finished Login (as Admin)

--- js/mainWindow.js
+++ js/mainWindow.js
@@ -1,5 +1,38 @@
 window.addEventListener('message', (evt) => {
         const userName = evt.data.userName;
         const email = evt.data.email;
+
+        loginMessageReceived(userName, email);
     }
 );
+
+function loginMessageReceived(userName, email) {
+    clearNavMenu();
+    addLinkInNavMenu('Patients');
+    addLinkInNavMenu('Doctors');
+    addLinkInNavMenu('Staff');
+    addLinkInNavMenu('Profile');
+
+    document.querySelector('#helloTitle').innerHTML += ' ' + userName;
+    document.querySelector('#helloTitle')
+            .classList
+            .remove('invisible');
+    document.querySelector('#helloTitle')
+            .classList
+            .add('visible');
+}
+
+function addLinkInNavMenu(title) {
+    document.querySelector('#navigationMenu').innerHTML +=
+        '<a class="mdl-navigation__link"  target="main-content">' + title + '</a>'
+
+}
+
+function clearNavMenu() {
+    document.querySelector('#navigationMenu').innerHTML = '';
+}
+
+
+
+
+
