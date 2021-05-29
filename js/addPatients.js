75666bb872a8fb3f0f846c53720e812abb49b83a
Satyam Bansal <sbansal1999@gmail.com>
2021-05-29 12:46:47 +0530

    Finished Add Patients Module

--- js/addPatients.js
+++ js/addPatients.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', init);
 const require = parent.require;
 const admin = require('firebase-admin');
-const {ipcRenderer} = require('electron');
-const firebase = require('firebase');
 let imageCaptured = false;
 
 function firebaseInit() {
@@ -18,13 +16,12 @@ function init() {
     attachCamera();
     firebaseInit();
 
-
     let captureImageButton = document.querySelector("#captureImage");
     captureImageButton.addEventListener('click', captureImage);
     captureImage('first');
 
     let retakeImage = document.querySelector('#retakeImage');
-    retakeImage.addEventListener('click', (e) => {
+    retakeImage.addEventListener('click', () => {
         attachCamera();
         retakeImage.disabled = true;
         captureImageButton.disabled = false;
@@ -33,7 +30,7 @@ function init() {
     retakeImage.disabled = true;
 
     document.querySelector('#register')
-            .addEventListener('click', (evt => {
+            .addEventListener('click', (() => {
                 let isValid = document.querySelector('#inputForm')
                                       .checkValidity();
                 if (isValid === true) {
@@ -43,17 +40,71 @@ function init() {
                     //TODO add some possible exceptions here
                 }
             }));
+
+    document.querySelector('#reset')
+            .addEventListener('click', () => {
+                resetForm();
+            });
+
+    fetchTodayList();
+}
+
+/**
+ * Method that retrieves data of patients that are registered today
+ */
+function fetchTodayList() {
+    clearTable();
+
+    const rootRef = admin.database()
+                         .ref();
+
+    const today = generatePatientID()
+    .substring(0, 8);
+
+    rootRef.child('patients_info')
+           .orderByChild('patientID')
+           .startAt(today)
+           .endAt(today + '\uf8ff')
+           .limitToFirst(5)
+           .once('value')
+           .then((snapshot) => {
+               if (snapshot.exists()) {
+                   snapshot.forEach((snap) => {
+                       addDataToTable(snap.val());
+                   });
+               } else {
+                   //No User Registered Today
+
+               }
+           })
+           .catch((err) => {
+
+           });
+
+    function clearTable() {
+        const todayTable = document.querySelector('#todayTableTBody');
+        todayTable.innerHTML = '';
+    }
+
+    function addDataToTable(val) {
+        const todayTable = document.querySelector('#todayTableTBody');
+
+        todayTable.classList.remove('invisible');
+
+        let row = todayTable.insertRow(-1);
+        row.insertCell(0).innerHTML = val.patientID;
+        row.insertCell(1).innerHTML = val.name;
+        row.insertCell(2).innerHTML = val.phone;
+        row.insertCell(3).innerHTML = val.residentialAddress;
+    }
 }
 
 function submitForm() {
-    setSpinnerState(true);
+    showToast("Registering");
 
     const patientDetails = getPatientDetails();
 
     addPatient(patientDetails);
-
-    console.log("details are");
-    console.log(patientDetails);
 
     function getPatientDetails() {
         return {
@@ -69,58 +120,63 @@ function submitForm() {
     }
 }
 
-/**
- * Adds Patient to the Firebase Auth as well as Realtime DB using Firebase Functions
- */
-function addPatient(details) {
-    let email;
-    let toCreate = true;
-
-    //Performs Email Check
-    admin.database()
-         .ref("patients_info")
-         .orderByChild('emailAddress')
-         .equalTo(details.email)
-         .once('value')
-         .then((snapshot) => {
-                 if (snapshot.exists()) {
-                     showToast("Email Already Registered 1"+ toCreate);
-                     toCreate = false;
-                     console.log(toCreate);
-                 }
-             }
-         )
-         .catch((error) => {
-             console.log(error);
-         });
-
-    if (toCreate === true) {
-        admin.auth()
-             .createUser({
-                 email: details.email,
-                 phoneNumber: "+91" + details.phoneNum,
-                 displayName: details.pName,
-             })
-             .then((user) => {
-                 addIntoDatabase(details, user.uid);
-                 console.log("User Created" + user.uid);
-             })
-             .catch((error) => {
-                 if (error.code === "auth/email-already-exists") {
-                     showToast("Email Already Registered");
-                 } else if (error.code === "auth/phone-number-already-exists") {
-                     showToast("Mobile Number Already Registered");
-                 } else {
-                     showToast("Something has gone wrong. Contact Support");
-                 }
-             });
-        setSpinnerState(false);
-    }
-
+function resetForm() {
+    document.querySelector('#inputForm')
+            .reset();
 }
 
 function uploadImage() {
 
+}
+
+function checkLengthAndAddZero(string, length) {
+    if (string.length === length) {
+        return '0' + string;
+    }
+    return string;
+}
+
+function retrieveTextFromID(id) {
+    return document.querySelector('#' + id)
+        .value;
+}
+
+function attachCamera() {
+    Webcam.set({
+        width: 400,
+        height: 250,
+        image_format: 'jpeg',
+        jpeg_quality: 100
+    });
+
+    Webcam.attach('#camera');
+    Webcam.on('error', () => {
+        //TODO add a loading image while the camera is still loading, image is in assets folder
+    });
+}
+
+function captureImage(msg) {
+    Webcam.snap((data_uri) => {
+        //Disables Camera and sets the Captured Image instead of it
+        Webcam.reset('#camera');
+
+        document.querySelector('#camera').innerHTML += '<img alt="Error" src="' + data_uri + '"/>';
+    });
+
+    if (msg !== 'first') {
+        document.querySelector("#captureImage").disabled = true;
+        imageCaptured = true;
+    }
+    document.querySelector('#retakeImage').disabled = false;
+}
+
+function showToast(message) {
+    let notification = document.querySelector('.mdl-js-snackbar');
+    notification.MaterialSnackbar.showSnackbar(
+        {
+            message: message
+        }
+    );
 }
 
 /**
@@ -165,57 +221,54 @@ function generatePatientID() {
 
 }
 
-function checkLengthAndAddZero(string, length) {
-    if (string.length === length) {
-        return '0' + string;
-    }
-    return string;
-}
+/**
+ * Adds Patient to the Firebase Auth as well as Realtime DB using Firebase Functions
+ */
+function addPatient(details) {
+    //Performs Email Check
+    admin.database()
+         .ref("patients_info")
+         .orderByChild('emailAddress')
+         .equalTo(details.email)
+         .once('value')
+         .then((snapshot) => {
+                 if (snapshot.exists()) {
+                     showToast("Email Already Registered 1");
+                 } else {
+                     admin.auth()
+                          .createUser({
+                              email: details.email,
+                              phoneNumber: "+91" + details.phoneNum,
+                              displayName: details.pName,
+                          })
+                          .then((user) => {
+                              addIntoDatabase(details, user.uid);
+                          })
+                          .catch((error) => {
+                              switch (error.code) {
+                                  case "auth/email-already-exists":
+                                      showToast("Email Already Registered");
+                                      break;
+                                  case "auth/phone-number-already-exists":
+                                      showToast("Mobile Number Already Registered");
+                                      break;
+                                  case "auth/invalid-phone-number":
+                                      showToast("Kindly recheck the Mobile Number");
+                                      break;
+                                  case "auth/invalid-email":
+                                      showToast("Kindly recheck the Email Address");
+                                      break;
+                                  default:
+                                      showToast("Something has gone wrong. Contact Support");
+                                      break;
+                              }
+                          });
+                 }
+             }
+         )
+         .catch((error) => {
+         });
 
-function retrieveTextFromID(id) {
-    return document.querySelector('#' + id)
-        .value;
-}
-
-function attachCamera() {
-    Webcam.set({
-        width: 400,
-        height: 250,
-        image_format: 'jpeg',
-        jpeg_quality: 100
-    });
-
-    Webcam.attach('#camera');
-    Webcam.on('error', () => {
-        console.log("Loading");
-
-        //TODO add a loading image while the camera is still loading, image is in assets folder
-    });
-}
-
-function captureImage(msg) {
-    Webcam.snap((data_uri) => {
-        //Disables Camera and sets the Captured Image instead of it
-        console.log("here");
-        Webcam.reset('#camera');
-
-        document.querySelector('#camera').innerHTML += '<img alt="Error" src="' + data_uri + '"/>';
-    });
-
-    if (msg !== 'first') {
-        document.querySelector("#captureImage").disabled = true;
-        imageCaptured = true;
-    }
-    document.querySelector('#retakeImage').disabled = false;
-}
-
-function showToast(message) {
-    let notification = document.querySelector('.mdl-js-snackbar');
-    notification.MaterialSnackbar.showSnackbar(
-        {
-            message: message
-        }
-    );
 }
 
 /**
@@ -246,25 +299,9 @@ function addIntoDatabase(data, uid) {
                       .set(obj);
     // Adds the obj to the Firebase Realtime Database
     snap.then(() => {
-        console.log("Data Inserted");
         showToast("Patient Added Successfully");
     })
         .catch((error) => {
-            console.log("error" + error);
         })
-
-}
-
-/**
- * Method that modifies the state of the spinner according to the given
- * @param state either true or false
- */
-function setSpinnerState(state) {
-    const prg = document.querySelector('#registerProgressBar');
-    if (state === true) {
-        prg.classList.add('is-active');
-    } else {
-        prg.classList.remove('is-active');
-    }
 
 }
